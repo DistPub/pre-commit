b218588684a5f659930ddf7ac5407b02d1197b1a
ZJ <569290339@qq.com>
2017-07-10 15:31:40 +0800

    add precommit repo

---
+++ dockconn.py
@@ -0,0 +1,30 @@
+import os
+import platform
+
+DEFAULT_WIN7_BASE_URL = "tcp://192.168.99.100:2376"
+DEFAULT_WIN_TLS_HOME = r"%USERPROFILE%\\.docker\\machine\\machines\\default\\"
+DEFAULT_CONTAINER_NAME = 'web_server_1'
+
+def exec_docker(cmd):
+    if platform.system() == 'Windows':
+        if platform.release() == '10':
+            exec_in_win10_docker(cmd)
+        else:
+            exec_in_win7_docker(cmd)
+    else:
+        exec_in_unix_docker(cmd)
+
+def exec_in_win7_docker(cmd):
+    return os.system(
+        'docker -H {url} --tls --tlscert {tls_home}cert.pem {tls_home}key.pem {cmd}'.format(
+        url=DEFAULT_WIN7_BASE_URL,
+        tls_home=DEFAULT_WIN7_BASE_URL,
+        cmd=cmd,
+        )
+    )
+
+def exec_in_win10_docker(cmd):
+    return os.system('docker {}'.format(cmd))
+
+def exec_in_unix_docker(cmd):
+    return os.system('docker {}'.format(cmd))
