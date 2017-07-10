8c44e2f8d866f5aeee575ea468de301a18b4956c
ZJ <569290339@qq.com>
2017-07-10 16:47:16 +0800

    add tag

--- dockconn.py
+++ dockconn.py
@@ -5,14 +5,13 @@ DEFAULT_WIN7_BASE_URL = "tcp://192.168.99.100:2376"
 DEFAULT_WIN_TLS_HOME = r"%USERPROFILE%\\.docker\\machine\\machines\\default\\"
 DEFAULT_CONTAINER_NAME = 'web_server_1'
 
-def exec_docker(cmd):
-    if platform.system() == 'Windows':
-        if platform.release() == '10':
-            exec_in_win10_docker(cmd)
-        else:
-            exec_in_win7_docker(cmd)
+if platform.system() == 'Windows':
+    if platform.release() == '10':
+        exec_docker = exec_in_win10_docker
     else:
-        exec_in_unix_docker(cmd)
+        exec_docker = exec_in_win7_docker
+else:
+    exec_docker = exec_in_unix_docker
 
 def exec_in_win7_docker(cmd):
     return os.system(
