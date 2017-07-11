a9529b0d22f2d62ea577741b2d9da927cb7e7dbe
ZJ <569290339@qq.com>
2017-07-11 17:19:14 +0800

    fix bug: return exit code 1

--- exec_in_docker.py
+++ exec_in_docker.py
@@ -1,7 +1,8 @@
-# !python
+#!/usr/bin/env python
 
 import sys
 
 from dockconn import exec_docker, DEFAULT_CONTAINER_NAME
 
-exec_docker('exec {} {}'.format(DEFAULT_CONTAINER_NAME, ' '.join(sys.argv[1:])))
+if exec_docker('exec {} {}'.format(DEFAULT_CONTAINER_NAME, ' '.join(sys.argv[1:]))):
+    sys.exit(1)
