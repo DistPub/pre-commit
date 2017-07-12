a7e37adb8610530fab93941cb71e91f917212424
ZJ <569290339@qq.com>
2017-07-12 16:44:07 +0800

    rm install script

--- install_git_pro_commit_hooks.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-import sys
-
-if len(sys.argv) == 1:
-    raise SystemExit('[ERROR]: Require working-space directory!')
-os.chdir(sys.argv[1])
-os.system('pre-commit install')
