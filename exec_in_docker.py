0c260d94a8851024664c7a63dbed0aa070c1d809
ZJ <569290339@qq.com>
2017-08-24 20:39:16 +0800

    fix es sort

--- exec_in_docker.py
+++ exec_in_docker.py
@@ -2,7 +2,7 @@
 
 import sys
 
-from dockconn import exec_docker, DEFAULT_CONTAINER_NAME
+from cubeadev.docker_conn import exec_docker
 
-if exec_docker('exec {} {}'.format(DEFAULT_CONTAINER_NAME, ' '.join(sys.argv[1:]))):
+if exec_docker(sys.argv[1:]):
     sys.exit(1)
